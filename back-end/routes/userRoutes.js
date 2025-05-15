125b2dd23f68a7e4191f349e4ce8047e3a997812
abencir <abencirmohmed@gmail.com>
2025-05-15 15:04:27 +0200

    Just add Send in login router

--- back-end/routes/userRoutes.js
+++ back-end/routes/userRoutes.js
@@ -69,7 +69,7 @@ router.post('/login', async (req,res) => {
             })
 
         }else{
-            res.status(500).send('Sira 3end inak')
+            res.status(500).send('Ghleti a m3lem')
         }
     }catch(err){
         res.status(400).send(err)

3725d7e2f53fbb7ae9f90e2a5f7c550d4086d7ae
abencir <abencirmohmed@gmail.com>
2025-05-19 10:57:52 +0200

    protect routs

--- back-end/routes/eventRoutes.js
+++ back-end/routes/eventRoutes.js
@@ -6,13 +6,15 @@ import {
   deleteEventByTitle,
   getEventByTitle,
 } from "../controllers/eventController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import adminAccess from "../middlewares/adminMiddelware.js";
 
 const router = express.Router();
 
-router.post("/", createEvent);
+router.post("/",authMiddleware, adminAccess ,createEvent);
 router.get("/", getEvents);
 router.get("/:title", getEventByTitle);
-router.put("/:title", updateEventByTitle);
-router.delete("/:title", deleteEventByTitle);
+router.put("/:title", authMiddleware, adminAccess ,updateEventByTitle);
+router.delete("/:title", authMiddleware, adminAccess , deleteEventByTitle);
 
 export default router;
--- back-end/routes/notifRoutes.js
+++ back-end/routes/notifRoutes.js
@@ -5,7 +5,8 @@ import {
     deleteNotif,
     createNotif
 } from '../controllers/notifController.js'
-
+import authMiddleware from "../middlewares/authMiddleware.js";
+import adminAccess from "../middlewares/adminMiddelware.js";
 const router = Router();
 
 // test create notif 
@@ -17,8 +18,8 @@ const router = Router();
 //       res.status(500).json({  error: err.message });
 //     }
 //   });
-router.get('/:userId', getNotif);
-router.patch('/:notifId',markAsRead);
-router.delete('/:notifId',deleteNotif);
+router.get('/:userId', authMiddleware,getNotif);
+router.patch('/:notifId', authMiddleware, adminAccess ,markAsRead);
+router.delete('/:notifId', authMiddleware, adminAccess ,deleteNotif);
 
 export default router;

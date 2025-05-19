b81f4de92fdc4e4b3f343fd5c867f06980b1b08c
abencir <abencirmohmed@gmail.com>
2025-05-19 11:09:24 +0200

    Rename event file

--- back-end/server.js
+++ back-end/server.js
@@ -1,37 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import EventRoutes from './routes/eventRoutes.js'
+import router from "./routes/EventRoutes.js";
 import courseRoutes from './routes/courseRoutes.js';
-<<<<<<< HEAD
-import adminrouters from "./routes/adminRouters.js";
-import router from "./routes/authRouters.js";
-=======
 import router from "./routes/userRoutes.js";
 import notifRoutes from "./routes/notifRoutes.js"
->>>>>>> faa4d64fbdb05adf4ff54b2ea2edf0dde4c4b55a
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
-<<<<<<< HEAD
-app.use('/api', router);
-app.use('/api', EventRoutes);
-app.use('/api', courseRoutes);
-app.use('/api/admin', adminrouters);
- 
-
-mongngoose.connect(process.env.PASSWORD_DB)
-=======
 app.use('/api/users', router);
 app.use('/api/events', EventRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/notifications', notifRoutes);
 
 mongoose.connect(process.env.PASSWORD_DB)
->>>>>>> faa4d64fbdb05adf4ff54b2ea2edf0dde4c4b55a
 .then (() => {
     console.log('Conected Successfully')
 }).catch((err)=>{

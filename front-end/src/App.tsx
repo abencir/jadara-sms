5ba5e590a3c318e3d0fe8ee7f5a2acc445cc7a7d
assahli hasna <hasnaassahli2@gmail.com>
2025-05-27 10:22:55 +0100

    fix conflicts

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,30 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/shared/Layout';
-import './App.css'
-import MainPage from './pages/landingpage'
-
+import './App.css';
+import MainPage from './pages/landingpage';
 import StudentDashboard from './pages/StudentDashboard';
 import LoginPage from './pages/login';
 import SignUpPage from './pages/signup';
 import CourseCrudPage from './components/CourseCrudPage';
 
 function App() {
-
-
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Layout>< MainPage/></Layout>}/>
-        <Route path='/' element={<Layout><div></div></Layout>}/>
-        <Route path='/login' element={<Layout><LoginPage /></Layout>}/>
-        <Route path='/register' element={<Layout><SignUpPage /></Layout>}/>
-        <Route path='/' element={<Layout><MainPage /></Layout>}/>
-        <Route path='/studentdashboard' element={<Layout><StudentDashboard/></Layout>}/>
-         <Route path='/CourseCrudPage' element={<Layout><CourseCrudPage/></Layout>}/>
+        <Route path='/' element={<Layout><MainPage /></Layout>} />
+        <Route path='/login' element={<Layout><LoginPage /></Layout>} />
+        <Route path='/register' element={<Layout><SignUpPage /></Layout>} />
+        <Route path='/studentdashboard' element={<Layout><StudentDashboard /></Layout>} />
+        <Route path='/CourseCrudPage' element={<Layout><CourseCrudPage /></Layout>} />
+        <Route path='/updateprofile' element={<Layout><UpdateProfile /></Layout>} />
       </Routes>
     </Router>
-
-  )
+  );
 }
 
-export default App
+export default App;
